94bddafa9f62abfda0d7e3644ff5785df7aee7d6
박민재 <sobu0715@gmail.com>
2019-03-23 00:36:55 +0900

    feat: Project Setting

---
+++ dcomhomepage/urls.py
@@ -0,0 +1,27 @@
+"""dcomhomepage URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('board/', include('board.urls')),
+    path('file/', include('file.urls')),
+    path('info/', include('info.urls')),
+    path('post/', include('post.urls')),
+    path('study/', include('study.urls')),
+    path('user/', include('user.urls'))
+]
