7268e7ff1bdea4bf77fcc538d1d8d8b95269c434
박민재 <sobu0715@gmail.com>
2019-03-28 19:59:55 +0900

    fix: Model Fix #3

--- user/views.py
+++ user/views.py
@@ -4,7 +4,23 @@ from django.shortcuts import render
 
 
 def signup(request):
-    return
+    if request.method == "GET":
+        if request.user.is_authenticated:
+            error_dict = {
+                "status_code": 403,
+                "message_title": "403 Error",
+                "message_context": "잘못 된 요청입니다."
+            }
+            return render(request, 'error.html', {}, status=403)
+
+        render(request, 'signup.html', )
+    else:
+        error_dict = {
+            "status_code": 405,
+            "message_title": "405 Error",
+            "message_context": "잘못 된 요청 방식입니다."
+        }
+        return render(request, 'error.html', {}, status=405)
 
 
 def login(request):
