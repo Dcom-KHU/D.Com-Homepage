a8af886fef10cc0d45b5322cf98e3af14ef495c5
박민재 <sobu0715@gmail.com>
2019-04-21 15:38:05 +0900

    feat: Post View #7 (dev)

--- post/form.py
+++ post/form.py
@@ -1,8 +1,48 @@
 from django import forms
+from post.models import PostNotice, PostShare, PostFree, PostJokbo, PostStudy
 from ckeditor_uploader.widgets import CKEditorUploadingWidget
 
 
-class PostForm(forms.Form):
-    title = forms.CharField()
+class PostNoticeForm(forms.ModelForm):
     content = forms.CharField(widget=CKEditorUploadingWidget)
+
+    class Meta:
+        model = PostNotice
+        fields = ['tag', 'title', 'content']
+
+
+class PostShareForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorUploadingWidget)
+
+    class Meta:
+        model = PostShare
+        fields = ['tag', 'title', 'content']
+
+
+class PostFreeForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorUploadingWidget)
+
+    class Meta:
+        model = PostFree
+        fields = ['title', 'content']
+
+
+class PostJokboForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorUploadingWidget)
+
+    class Meta:
+        model = PostJokbo
+        fields = ['tag', 'title', 'content']
+
+
+class PostStudyForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorUploadingWidget)
+
+    class Meta:
+        model = PostStudy
+        fields = ['tag', 'title', 'content']
+
+
+class CommentForm(forms.Form):
+    comment = forms.CharField(widget=CKEditorUploadingWidget)
 
--- post/views.py
+++ post/views.py
@@ -3,8 +3,8 @@ from django.core.exceptions import PermissionDenied
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
 from user.models import Profile
-from post.models import PostNotice, PostFree, PostJokbo
-from post.form import PostForm
+from post.models import PostNotice, PostFree, PostJokbo, PostShare
+from post.form import PostNoticeForm, PostJokboForm, PostFreeForm, PostShareForm, PostStudyForm, CommentForm
 
 
 def notice_detail(request, post_id):
@@ -39,7 +39,7 @@ def notice_detail(request, post_id):
             return render(request, 'notice_post.html', {
                 'post': post_obj,
                 'comments': list_var,
-                'form': PostForm()
+                'commentForm': CommentForm()
             })
         else:
             raise PermissionDenied
@@ -60,12 +60,65 @@ def notice_list(request, page=1):
     })
 
 
+def share_detail(request, post_id):
+    try:
+        post_obj = PostShare.objects.get(pk=post_id)
+        post_obj.hit += 1
+        post_obj.save()
+
+        if post_obj.parent is None:
+            comment = PostShare.objects.filter(parent=post_obj).values()
+            list_var = list(comment)
+            depth = 0
+
+            while True:
+                temp = 0
+                depth += 1
+                check = False
+
+                for i in range(len(list_var)):
+                    if list_var[i]['depth'] == depth:
+                        check = True
+                        sub_comment = list(PostShare.objects.filter(parent_id=list_var[i]['id']).values())
+
+                        list_var = list_var[:i+temp+1] + sub_comment + list_var[i+temp+1:]
+                        temp += len(sub_comment)
+
+                if check == False:
+                    break
+
+            print(list_var)
+
+            return render(request, 'notice_post.html', {
+                'post': post_obj,
+                'comments': list_var,
+                'form': PostNoticeForm()
+            })
+        else:
+            raise PermissionDenied
+    except PostNotice.DoesNotExist:
+        raise Http404
+
+
+def share_list(request, page=1):
+    post_obj = PostShare.objects.filter(parent=None).order_by('-id')
+    obj_num = post_obj.count()
+
+    pages = ((obj_num-1) // 5) + 1
+    list_var = post_obj[(page-1)*5:page*5]
+    return render(request, 'notice_list.html', {
+        'posts': list_var,
+        'page': page,
+        'pages': pages
+    })
+
+
 @login_required
 def free_detail(request, post_id):
     profile = Profile.objects.get(pk=request.user)
 
     if profile.isVerified == False:
-        return PermissionDenied
+        raise PermissionDenied
 
     try:
         post_obj = PostFree.objects.get(pk=post_id)
@@ -110,7 +163,7 @@ def free_list(request, page=1):
     profile = Profile.objects.get(pk=request.user)
 
     if profile.isVerified == False:
-        return PermissionDenied
+        raise PermissionDenied
 
     post_obj = PostFree.objects.filter(parent=None).order_by('-id')
     obj_num = post_obj.count()
@@ -129,7 +182,7 @@ def jokbo_detail(request, post_id):
     profile = Profile.objects.get(pk=request.user)
 
     if profile.isVerified == False:
-        return PermissionDenied
+        raise PermissionDenied
 
     try:
         post_obj = PostJokbo.objects.get(pk=post_id)
@@ -174,7 +227,7 @@ def jokbo_list(request, page=1):
     profile = Profile.objects.get(pk=request.user)
 
     if profile.isVerified == False:
-        return PermissionDenied
+        raise PermissionDenied
 
     post_obj = PostJokbo.objects.filter(parent=None).order_by('-id')
     obj_num = post_obj.count()
@@ -188,13 +241,53 @@ def jokbo_list(request, page=1):
     })
 
 @login_required
-def notice_post(request, parents=0):
+def notice_post(request, parents=-1):
+    profile = Profile.objects.get(pk=request.user)
+
+    if profile.isVerified == False:
+        raise PermissionDenied
+
+    if parents == -1:
+        params = ""
+    else:
+        params = str(parents)
+
+    if request.method == "POST":
+        try:
+            require_keys = ('title', 'content')
+            if all(i in request.POST for i in require_keys):
+                if parents != -1:
+                    parentPost = PostNotice.objects.get(pk=parents)
+                    postobj = PostNotice.objects.create(
+                        title=request.POST['title'],
+                        content=request.POST['contents'],
+                        userIdx=request.user,
+                        tag=request.POST['tag'],
+                        parent=parentPost
+                    )
+                else:
+                    postobj = PostNotice.objects.create(
+                        title=request.POST['title'],
+                        content=request.POST['contents'],
+                        userIdx=request.user,
+                        tag=request.POST['tag']
+                    )
+        except PostNotice.DoesNotExist:
+            pass
+    else:
+        return render(request, "posts.html", {
+            'link': '/post/notice/write/' + params,
+            'form': PostNoticeForm()
+        })
+
+@login_required
+def free_post(request, parents=-1):
     return
 
 @login_required
-def free_post(request, parents=0):
+def jokbo_post(request, parents=-1):
     return
 
 @login_required
-def jokbo_post(request, parents=0):
+def share_post(request, parents=-1):
     return
